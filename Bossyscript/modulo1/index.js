234ec576b6f48077516bd7e524c1d71fdff28274
Jessie Moura <jessie.moura19@gmail.com>
2023-03-04 18:14:58 -0300

    starting to study

---
+++ Bossyscript/modulo1/index.js
@@ -0,0 +1,7 @@
+var firstName = "Jessica";
+var secondName = 'Isabel';
+function greeting(name) {
+    console.log('Olá, ', name);
+}
+greeting(firstName);
+greeting(secondName);
---
+++ Bossyscript/modulo1/index.ts
@@ -0,0 +1,8 @@
+const firstName = "Jessica"
+const secondName = 'Isabel'
+
+function greeting(name:string){
+    console.log('Olá, ', name)
+}
+greeting(firstName)
+greeting(secondName)
